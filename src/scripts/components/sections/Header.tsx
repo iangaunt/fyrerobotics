2498257c895ebc05463ff5f88c5acc313146c934
Ian G <66448861+iangaunt@users.noreply.github.com>
2024-01-21 16:14:21 -0800

    Rebuilt the website using a React-Sass toolchain

---
+++ src/scripts/components/sections/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header id="full-header">
+            <a id="logo">
+                <img src="711ebe8d03035220e9db.svg"></img>
+            </a>
+            <hr></hr>
+
+            <a href="#about-us">About Us</a>
+            <a href="#robots">Robots</a>
+            <a href="#contact">Sponsors</a>
+            <a href="#footer">Contact Us</a>
+        </header>
+    )
+}
---
+++ src/scripts/components/sections/Title.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Title() {
+    return (
+        <div id="title">
+            <div>
+                <h1>FYRE 5480</h1>
+                <p>FRC team based in Reno, Nevada</p>
+            </div>
+            <img src="./711ebe8d03035220e9db.svg"></img>
+        </div>
+    )
+}
